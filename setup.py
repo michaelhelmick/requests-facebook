cb6d8b560427057ee489d1aa2575ac2b231b7487
Markus Kaiserswerth <github@sensun.org>
2014-01-30 12:49:03 +0100

    v0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='requests-facebook',
-    version='0.2.0',
+    version='0.2.1',
     install_requires=['requests>=1.0.0'],
     author='Mike Helmick',
     author_email='me@michaelhelmick.com',
