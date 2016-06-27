4d1d4c6730c2d09ec387267f5eebbcc24f70ed38
Mike Helmick <me@michaelhelmick.com>
2016-06-27 12:18:40 -0400

    Upgrade requests-facebook version to 0.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='requests-facebook',
-    version='0.3.0',
+    version='0.4.0',
     install_requires=['requests>=1.0.0'],
     author='Mike Helmick',
     author_email='me@michaelhelmick.com',
